77f41b8f2334a2748c644f1c37d95d8a630735d7
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-12-29 07:03:48 -0500

    Track voxels that were foreground and merged

--- src/main/kotlin/net/imglib2/algorithm/labeling/affinities/ConnectedComponents.kt
+++ src/main/kotlin/net/imglib2/algorithm/labeling/affinities/ConnectedComponents.kt
@@ -20,10 +20,11 @@ class ConnectedComponents {
 	companion object {
 
 		@JvmStatic
-		fun <B: BooleanType<B>, R: RealType<R>, C: Composite<R>, L: IntegerType<L>> fromSymmetricAffinities(
+		fun <B: BooleanType<B>, U: BooleanType<U>, R: RealType<R>, C: Composite<R>, L: IntegerType<L>> fromSymmetricAffinities(
 				foreground: RandomAccessible<B>,
 				affinities: RandomAccessible<C>,
 				labels: RandomAccessibleInterval<L>,
+				unionMask: RandomAccessible<U>,
 				threshold: Double,
 				vararg steps: Long,
 				unionFind: UnionFind = IntArrayUnionFind(Intervals.numElements(labels).toInt()),
@@ -34,21 +35,23 @@ class ConnectedComponents {
 				return fromSymmetricAffinities(
 						Views.translate(foreground, *Intervals.minAsLongArray(labels).invertValues()),
 						Views.translate(affinities, *Intervals.minAsLongArray(labels).invertValues()),
-						labels,
+						Views.zeroMin(labels),
+						Views.translate(unionMask, *Intervals.minAsLongArray(labels).invertValues()),
 						threshold,
 						*steps,
 						unionFind = unionFind,
 						toIndex = toIndex)
 
-			unionFindFromSymmetricAffinities(foreground, Views.interval(affinities, labels), unionFind, threshold, *steps, toIndex = toIndex)
-			relabel(Views.interval(foreground, labels), labels, unionFind, toIndex)
+			unionFindFromSymmetricAffinities(foreground, Views.interval(affinities, labels), unionMask, unionFind, threshold, *steps, toIndex = toIndex)
+			relabel(Views.interval(foreground, labels), labels, Views.interval(unionMask, labels), unionFind, toIndex)
 		}
 
 		@JvmStatic
-		fun <B: BooleanType<B>, R: RealType<R>, C: Composite<R>, L: IntegerType<L>> fromSymmetricAffinities(
+		fun <B: BooleanType<B>, U: BooleanType<U>, R: RealType<R>, C: Composite<R>, L: IntegerType<L>> fromSymmetricAffinities(
 				foreground: RandomAccessible<B>,
 				affinities: RandomAccessible<C>,
 				labels: RandomAccessibleInterval<L>,
+				unionMask: RandomAccessible<U>,
 				threshold: Double,
 				vararg steps: LongArray,
 				unionFind: UnionFind = IntArrayUnionFind(Intervals.numElements(labels).toInt()),
@@ -59,30 +62,35 @@ class ConnectedComponents {
 				return fromSymmetricAffinities(
 						Views.translate(foreground, *Intervals.minAsLongArray(labels).invertValues()),
 						Views.translate(affinities, *Intervals.minAsLongArray(labels).invertValues()),
-						labels,
+						Views.zeroMin(labels),
+						Views.translate(unionMask, *Intervals.minAsLongArray(labels).invertValues()),
 						threshold,
 						*steps,
 						unionFind = unionFind,
 						toIndex = toIndex)
 
-			unionFindFromSymmetricAffinities(foreground, Views.interval(affinities, labels), unionFind, threshold, *steps, toIndex = toIndex)
-			return relabel(Views.interval(foreground, labels), labels, unionFind, toIndex)
+			unionFindFromSymmetricAffinities(foreground, Views.interval(affinities, labels), unionMask, unionFind, threshold, *steps, toIndex = toIndex)
+			return relabel(Views.interval(foreground, labels), labels, Views.interval(unionMask, labels), unionFind, toIndex)
 		}
 
 		@JvmStatic
-		fun <B: BooleanType<B>, L: IntegerType<L>> relabel(
+		fun <B: BooleanType<B>, U: BooleanType<U>, L: IntegerType<L>> relabel(
 				mask: RandomAccessibleInterval<B>,
 				labels: RandomAccessibleInterval<L>,
+				unionMask: RandomAccessibleInterval<U>,
 				unionFind: UnionFind,
 				toIndex: (Localizable) -> Long,
 				indexToId: (Long) -> Long = {it+1})
 		: Long {
 			val c = Views.flatIterable(labels).cursor()
 			val b = Views.flatIterable(mask).cursor()
+			val u = Views.flatIterable(unionMask).cursor()
 			var maxId = Long.MIN_VALUE
 			while (c.hasNext()) {
 				val p = c.next()
-				if (b.next().get()) {
+				b.fwd()
+				u.fwd()
+				if (b.get().get() && u.get().get()) {
 					val id = indexToId(unionFind.findRoot(toIndex(c)))
 					p.setInteger(id)
 					if (id > maxId)
@@ -93,9 +101,10 @@ class ConnectedComponents {
 		}
 
 		@JvmStatic
-		fun <B: BooleanType<B>, R: RealType<R>, C: Composite<R>> unionFindFromSymmetricAffinities(
+		fun <B: BooleanType<B>, U: BooleanType<U>, R: RealType<R>, C: Composite<R>> unionFindFromSymmetricAffinities(
 				foreground: RandomAccessible<B>,
 				affinities: RandomAccessibleInterval<C>,
+				unionMask: RandomAccessible<U>,
 				unionFind: UnionFind,
 				threshold: Double,
 				vararg steps: Long,
@@ -107,11 +116,19 @@ class ConnectedComponents {
 						"and dimensions ${Arrays.toString(Intervals.dimensionsAsLongArray(affinities))}")
 
 			for (dim in steps.indices) {
+
 				val currentPixelMask = Views.interval(foreground, affinities)
 				val shiftedPixelMask = Views.interval(foreground, Intervals.translate(affinities, steps[dim], dim))
+
+				val currentUnionFindMask = Views.interval(unionMask, affinities)
+				val shiftedUnionFindMask = Views.interval(unionMask, Intervals.translate(affinities, steps[dim], dim))
+
 				val cCursor = Views.flatIterable(currentPixelMask).cursor()
 				val sCursor = Views.flatIterable(shiftedPixelMask).cursor()
 				val aCursor = Views.flatIterable(affinities).cursor()
+
+				val cuCursor = Views.flatIterable(currentUnionFindMask).cursor()
+				val suCursor = Views.flatIterable(shiftedUnionFindMask).cursor()
 
 				val dimAslong = dim.toLong()
 
@@ -119,6 +136,8 @@ class ConnectedComponents {
 					cCursor.fwd()
 					sCursor.fwd()
 					aCursor.fwd()
+					cuCursor.fwd()
+					suCursor.fwd()
 
 					val c = cCursor.get().get()
 					if (!c)
@@ -135,8 +154,11 @@ class ConnectedComponents {
 					val r1 = unionFind.findRoot(toIndex(cCursor))
 					val r2 = unionFind.findRoot(toIndex(sCursor))
 
-					if (r1 != r2)
+					if (r1 != r2) {
 						unionFind.join(r1, r2)
+						cuCursor.get().set(true)
+						suCursor.get().set(true)
+					}
 
 				}
 
@@ -145,9 +167,10 @@ class ConnectedComponents {
 		}
 
 		@JvmStatic
-		fun <B: BooleanType<B>, R: RealType<R>, C: Composite<R>> unionFindFromSymmetricAffinities(
+		fun <B: BooleanType<B>, U: BooleanType<U>, R: RealType<R>, C: Composite<R>> unionFindFromSymmetricAffinities(
 				foreground: RandomAccessible<B>,
 				affinities: RandomAccessibleInterval<C>,
+				unionMask: RandomAccessible<U>,
 				unionFind: UnionFind,
 				threshold: Double,
 				vararg steps: LongArray,
@@ -160,11 +183,19 @@ class ConnectedComponents {
 							"and dimensions ${Arrays.toString(Intervals.dimensionsAsLongArray(affinities))}")
 
 			for (stepIndex in steps.indices) {
+
 				val currentPixelMask = Views.interval(foreground, affinities)
 				val shiftedPixelMask = Views.interval(foreground, Views.translate(affinities, *steps[stepIndex]))
+
+				val currentUnionFindMask = Views.interval(unionMask, affinities)
+				val shiftedUnionFindMask = Views.interval(unionMask, Views.translate(affinities, *steps[stepIndex]))
+
 				val cCursor = Views.flatIterable(currentPixelMask).cursor()
 				val sCursor = Views.flatIterable(shiftedPixelMask).cursor()
 				val aCursor = Views.flatIterable(affinities).cursor()
+
+				val cuCursor = Views.flatIterable(currentUnionFindMask).cursor()
+				val suCursor = Views.flatIterable(shiftedUnionFindMask).cursor()
 
 				val stepIndexLong = stepIndex.toLong()
 
@@ -172,6 +203,8 @@ class ConnectedComponents {
 					cCursor.fwd()
 					sCursor.fwd()
 					aCursor.fwd()
+					cuCursor.fwd()
+					suCursor.fwd()
 
 					val c = cCursor.get().get()
 					if (!c)
@@ -188,8 +221,11 @@ class ConnectedComponents {
 					val r1 = unionFind.findRoot(toIndex(cCursor))
 					val r2 = unionFind.findRoot(toIndex(sCursor))
 
-					if (r1 != r2)
+					if (r1 != r2) {
 						unionFind.join(r1, r2)
+						cuCursor.get().set(true)
+						suCursor.get().set(true)
+					}
 
 				}
 
