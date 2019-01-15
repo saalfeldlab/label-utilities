1fe0bcc5735d138e55b07f628ef60b43b44fa223
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-01-15 13:11:44 -0500

    Reduce code duplication and optimizie toIndex parameter

--- src/main/kotlin/net/imglib2/algorithm/labeling/affinities/ConnectedComponents.kt
+++ src/main/kotlin/net/imglib2/algorithm/labeling/affinities/ConnectedComponents.kt
@@ -8,17 +8,34 @@ import net.imglib2.algorithm.util.unionfind.UnionFind
 import net.imglib2.type.BooleanType
 import net.imglib2.type.numeric.IntegerType
 import net.imglib2.type.numeric.RealType
-import net.imglib2.util.IntervalIndexer
 import net.imglib2.util.Intervals
 import net.imglib2.view.Views
 import net.imglib2.view.composite.Composite
+import org.slf4j.LoggerFactory
+import java.lang.invoke.MethodHandles
 import java.util.Arrays
 import java.util.function.LongUnaryOperator
-import java.util.function.ToLongFunction
 
 class ConnectedComponents {
 
+	@FunctionalInterface
+	interface ToIndex {
+
+		fun toIndex(position: Localizable, flatIndex: Long): Long
+
+	}
+
 	companion object {
+
+		private val LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+
+		private fun getDefaultToIndex(): ToIndex {
+			return object : ToIndex {
+				override fun toIndex(position: Localizable, flatIndex: Long): Long {
+					return flatIndex
+				}
+			}
+		}
 
 		@JvmStatic
 		@JvmOverloads
@@ -31,23 +48,22 @@ class ConnectedComponents {
 				vararg steps: Long,
 				indexToId: LongUnaryOperator = LongUnaryOperator { it + 1 },
 				unionFind: UnionFind = IntArrayUnionFind(Intervals.numElements(labels).toInt()),
-				toIndex: ToLongFunction<Localizable> = ToLongFunction{ IntervalIndexer.positionToIndex(it, labels) }
+				toIndex: ToIndex = getDefaultToIndex()
 		): Long {
 
-			if (!Views.isZeroMin(labels))
-				return fromSymmetricAffinities(
-						Views.translate(foreground, *Intervals.minAsLongArray(labels).invertValues()),
-						Views.translate(affinities, *Intervals.minAsLongArray(labels).invertValues()),
-						Views.zeroMin(labels),
-						Views.translate(unionMask, *Intervals.minAsLongArray(labels).invertValues()),
-						threshold,
-						*steps,
-						indexToId = indexToId,
-						unionFind = unionFind,
-						toIndex = toIndex)
+			val longArraySteps = steps.mapIndexed { index, l -> LongArray(foreground.numDimensions(), {if (it == index) l else 0}) }.toTypedArray()
 
-			unionFindFromSymmetricAffinities(foreground, Views.interval(affinities, labels), unionMask, unionFind, threshold, *steps, toIndex = toIndex)
-			return relabel(Views.interval(foreground, labels), labels, Views.interval(unionMask, labels), unionFind, toIndex, indexToId)
+			return fromSymmetricAffinities(
+					foreground = foreground,
+					affinities = affinities,
+					labels = labels,
+					unionMask = unionMask,
+					threshold = threshold,
+					steps = *longArraySteps,
+					indexToId = indexToId,
+					unionFind = unionFind,
+					toIndex = toIndex
+			);
 		}
 
 		@JvmStatic
@@ -60,7 +76,7 @@ class ConnectedComponents {
 				threshold: Double,
 				vararg steps: LongArray,
 				indexToId: LongUnaryOperator = LongUnaryOperator { it + 1 },
-				toIndex: ToLongFunction<Localizable> = ToLongFunction{ IntervalIndexer.positionToIndex(it, labels) },
+				toIndex: ToIndex = getDefaultToIndex(),
 				unionFind: UnionFind = IntArrayUnionFind(Intervals.numElements(labels).toInt())
 		): Long {
 
@@ -86,19 +102,21 @@ class ConnectedComponents {
 				labels: RandomAccessibleInterval<L>,
 				unionMask: RandomAccessibleInterval<U>,
 				unionFind: UnionFind,
-				toIndex: ToLongFunction<Localizable>,
+				toIndex: ToIndex,
 				indexToId: LongUnaryOperator)
 		: Long {
 			val c = Views.flatIterable(labels).cursor()
 			val b = Views.flatIterable(mask).cursor()
 			val u = Views.flatIterable(unionMask).cursor()
 			var maxId = Long.MIN_VALUE
+			var index = -1L
 			while (c.hasNext()) {
 				val p = c.next()
+				++index
 				b.fwd()
 				u.fwd()
 				if (b.get().get() && u.get().get()) {
-					val id = indexToId.applyAsLong(unionFind.findRoot(toIndex.applyAsLong(c)))
+					val id = indexToId.applyAsLong(unionFind.findRoot(toIndex.toIndex(c, index)))
 					p.setInteger(id)
 					if (id > maxId)
 						maxId = id
@@ -114,80 +132,20 @@ class ConnectedComponents {
 				unionMask: RandomAccessible<U>,
 				unionFind: UnionFind,
 				threshold: Double,
-				vararg steps: Long,
-				toIndex: ToLongFunction<Localizable>
-		) {
-
-			if (steps.size != affinities.numDimensions())
-				throw IllegalArgumentException("Need on step size for each dimension but got steps ${Arrays.toString(steps)} " +
-						"and dimensions ${Arrays.toString(Intervals.dimensionsAsLongArray(affinities))}")
-
-			for (dim in steps.indices) {
-
-				val currentPixelMask = Views.interval(foreground, affinities)
-				val shiftedPixelMask = Views.interval(foreground, Intervals.translate(affinities, steps[dim], dim))
-
-				val currentUnionFindMask = Views.interval(unionMask, affinities)
-				val shiftedUnionFindMask = Views.interval(unionMask, Intervals.translate(affinities, steps[dim], dim))
-
-				val cCursor = Views.flatIterable(currentPixelMask).cursor()
-				val sCursor = Views.flatIterable(shiftedPixelMask).cursor()
-				val aCursor = Views.flatIterable(affinities).cursor()
-
-				val cuCursor = Views.flatIterable(currentUnionFindMask).cursor()
-				val suCursor = Views.flatIterable(shiftedUnionFindMask).cursor()
-
-				val dimAslong = dim.toLong()
-
-				while (aCursor.hasNext()) {
-					cCursor.fwd()
-					sCursor.fwd()
-					aCursor.fwd()
-					cuCursor.fwd()
-					suCursor.fwd()
-
-					val c = cCursor.get().get()
-					if (!c)
-						continue
-
-					val s = sCursor.get().get()
-					if (!s)
-						continue
-
-					val a = aCursor.get().get(dimAslong).realDouble
-					if (a.isNaN() || a < threshold)
-						continue
-
-					val r1 = unionFind.findRoot(toIndex.applyAsLong(cCursor))
-					val r2 = unionFind.findRoot(toIndex.applyAsLong(sCursor))
-
-					if (r1 != r2) {
-						unionFind.join(r1, r2)
-						cuCursor.get().set(true)
-						suCursor.get().set(true)
-					}
-
-				}
-
-			}
-
-		}
-
-		@JvmStatic
-		fun <B: BooleanType<B>, U: BooleanType<U>, R: RealType<R>, C: Composite<R>> unionFindFromSymmetricAffinities(
-				foreground: RandomAccessible<B>,
-				affinities: RandomAccessibleInterval<C>,
-				unionMask: RandomAccessible<U>,
-				unionFind: UnionFind,
-				threshold: Double,
 				vararg steps: LongArray,
-				toIndex: ToLongFunction<Localizable>
+				toIndex: ToIndex
 		) {
 
 			for (step in steps)
 				if (step.size != affinities.numDimensions())
 					throw IllegalArgumentException("Need on step size for each dimension but got steps ${Arrays.toString(steps)} " +
 							"and dimensions ${Arrays.toString(Intervals.dimensionsAsLongArray(affinities))}")
+
+			val imgStrides = LongArray(affinities.numDimensions(), {1})
+			(1 until imgStrides.size).forEach { imgStrides[it] = imgStrides[it - 1] * affinities.dimension(it - 1) }
+			val flatIndexStrides = steps.map { it.zip(imgStrides).map { it.first * it.second }.sum() }
+			LOG.debug("Dimensions {} and strides {} and flat index strides {}", Intervals.dimensionsAsLongArray(affinities), imgStrides, flatIndexStrides)
+
 
 			for (stepIndex in steps.indices) {
 
@@ -205,6 +163,8 @@ class ConnectedComponents {
 				val suCursor = Views.flatIterable(shiftedUnionFindMask).cursor()
 
 				val stepIndexLong = stepIndex.toLong()
+				var cFlatIndex = -1L
+				var sFlatIndex = cFlatIndex + flatIndexStrides[stepIndex]
 
 				while (aCursor.hasNext()) {
 					cCursor.fwd()
@@ -212,6 +172,8 @@ class ConnectedComponents {
 					aCursor.fwd()
 					cuCursor.fwd()
 					suCursor.fwd()
+					++cFlatIndex
+					++sFlatIndex
 
 					val c = cCursor.get().get()
 					if (!c)
@@ -225,8 +187,8 @@ class ConnectedComponents {
 					if (a.isNaN() || a < threshold)
 						continue
 
-					val r1 = unionFind.findRoot(toIndex.applyAsLong(cCursor))
-					val r2 = unionFind.findRoot(toIndex.applyAsLong(sCursor))
+					val r1 = unionFind.findRoot(toIndex.toIndex(cCursor, cFlatIndex))
+					val r2 = unionFind.findRoot(toIndex.toIndex(sCursor, sFlatIndex))
 
 					if (r1 != r2) {
 						unionFind.join(r1, r2)
